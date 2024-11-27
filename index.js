52ff80016e4466dc314ad23ea2a600ca782eafbd
Rawad-Salaima <rawadsalaima4@gmail.com>
2024-11-27 16:38:35 +0100

    function för att skapa en tabell av databasen

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function calculateDistance(lat1, lon1, lat2, lon2) {
     Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * Math.sin(dLon / 2) ** 2;
   return (2 * R * Math.asin(Math.sqrt(a))) / 1.609;
 }
+
+function generateTable(cities) {
+    
+}
