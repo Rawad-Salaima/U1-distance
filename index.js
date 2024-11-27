cb40599d9e6c741eb29467b26516d2ab2d598054
Rawad-Salaima <rawadsalaima4@gmail.com>
2024-11-27 16:35:51 +0100

    constaster som jag ska använda

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371;
+    const toRad = (angle) => angle * (Math.PI / 180);
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
 }
